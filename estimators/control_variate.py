Fix variance: antithetic and cv + fix runs

estimators/control_variate.py
@@ -1,15 +1,20 @@
-import numpy as np
-import numpy.typing as npt
-
 from estimators.crude_monte_carlo import cmc_estimator
 from estimators.variance_estimators import covariance_estimator, variance_estimator
+
+import numpy as np
+import numpy.typing as npt
+from typing import Tuple
 
 
 # control variate estimator
 # X = [X_1, X_2, ..., X_n], Y = [Y_1, Y_2, ..., Y_n], where (X_1, Y_1), ..., (X_n, Y_n) are i.i.d. pairs
 # EX – expected value of X should be known
-def control_variate_estimator(X: npt.NDArray[np.float64], Y: npt.NDArray[np.float64], EX: float) -> float:
-	covXY = covariance_estimator(X, Y)
+def control_variate_estimator(X: npt.NDArray[np.float64], Y: npt.NDArray[np.float64], EX: float) -> Tuple[float, float]:
+	Y_cmc, var_Y_cmc = cmc_estimator(Y)
+	X_cmc, _ = cmc_estimator(X)
+	varY = variance_estimator(Y)
 	varX = variance_estimator(X)
+	covXY = covariance_estimator(X, Y)
 	c = -covXY / varX
-	return cmc_estimator(Y) + c * (cmc_estimator(X) - EX)
+	corr = covXY / np.sqrt(varX * varY)
+	return Y_cmc + c * (X_cmc - EX), var_Y_cmc * (1 - corr**2)

run_estimations/antithetic.py
@@ -6,7 +6,6 @@ from estimators.antithetic_estimator import antithetic_estimator
 from estimators.variance_estimators import variance_estimator, covariance_estimator
 
 import numpy as np
-import numpy.typing as npt
 from typing import Tuple
 
 
@@ -28,6 +27,6 @@ def run_antithetic(R: int) -> Tuple[float, float]:
 
 	result, sigma_cmc = antithetic_estimator(np.append(Y1, Y2))
 
-	sigma = sigma_cmc * (1 - corr)
+	sigma = sigma_cmc * (1 + corr)
 
 	return result, sigma

run_estimations/n1.py
@@ -15,12 +15,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-__no_tests = 100
 __n = 1
 
 
 def run_n_1():
-	global __no_tests, __n
+	global __n
 
 	variance_cmc = np.zeros_like(p.Rs, dtype=np.float64)
 	variance_stratified_proportional = np.zeros_like(p.Rs, dtype=np.float64)
@@ -40,19 +39,17 @@ def run_n_1():
 		stratified_R_optimal = np.ceil(R * stdev_strat_prop / np.sum(stdev_strat_prop)).astype(np.int32)
 		results_stratified_optimal, variance_stratified_optimal[i], _ = run_stratified(__n, stratified_R_optimal, p.m)
 
-		results_antithetic = run_antithetic(__no_tests, R)
-		variance_antithetic[i] = variance_estimator(results_antithetic)
+		results_antithetic, variance_antithetic[i] = run_antithetic(R)
 
-		results_cv = run_control_variate(__no_tests, R)
-		variance_cv[i] = variance_estimator(results_cv)
+		results_cv, variance_cv[i] = run_control_variate(R)
 
 	_, ax = plt.subplots(1, 1)
 
-	ax.plot(np.full_like(results_cmc, 1), results_cmc, label="Monte Carlo estimation", **plt_styles.monte_carlo_estimation)
-	ax.plot(np.full_like(results_stratified_proportional, 2), results_stratified_proportional, **plt_styles.monte_carlo_estimation)
-	ax.plot(np.full_like(results_stratified_optimal, 3), results_stratified_optimal, **plt_styles.monte_carlo_estimation)
-	ax.plot(np.full_like(results_antithetic, 4), results_antithetic, **plt_styles.monte_carlo_estimation)
-	ax.plot(np.full_like(results_cv, 5), results_cv, **plt_styles.monte_carlo_estimation)
+	ax.plot(1, results_cmc, label="Monte Carlo estimation", **plt_styles.monte_carlo_estimation)
+	ax.plot(2, results_stratified_proportional, **plt_styles.monte_carlo_estimation)
+	ax.plot(3, results_stratified_optimal, **plt_styles.monte_carlo_estimation)
+	ax.plot(4, results_antithetic, **plt_styles.monte_carlo_estimation)
+	ax.plot(5, results_cv, **plt_styles.monte_carlo_estimation)
 
 	ax.plot([1, 1], calculate_confidence_interval(results_cmc, variance_cmc[last_R_index]), label="confidence interval", **plt_styles.confidence_interval)
 	ax.plot([2, 2], calculate_confidence_interval(results_stratified_proportional, variance_stratified_proportional[last_R_index]), **plt_styles.confidence_interval)

run_estimations/stratified.py
@@ -33,7 +33,7 @@ def run_stratified(n: int, R: npt.NDArray[np.int32], m: int) -> Tuple[float, flo
 			trajectory = sample_trajectory(n, m, i + 1)
 			Y[r] = call_price(trajectory)
 
-		I[i] = cmc_estimator(Y)
+		I[i], _ = cmc_estimator(Y)
 		strata_sigma[i] = variance_estimator(Y)
 
 	result = stratified_estimator(p, I)